fedd1368c226d5887c8ad36d73b404199e0d922f
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-03 16:54:03 +0530

    Part-1 finished

---
+++ Basics/mathFunction.js
@@ -0,0 +1,22 @@
+console.log(Math.random()); // generates a random number between 0  and 1
+
+console.log(Math.random() * 10 + 1); // generates a random number between 1  and 10
+
+console.log(Math.floor(Math.random() * 10 + 1)); // generates a random integer between 1 and 10
+
+console.log(Math.ceil(4.2)); // 5 --> rounds up to the nearest integer
+
+console.log(Math.floor(4.7)); // 4 --> rounds down to the nearest integer
+
+console.log(Math.round(4.5)); // 5 --> rounds to the nearest integer
+console.log(Math.round(4.4)); // 4 --> rounds to the nearest integer
+
+console.log(Math.trunc(4.7)); // 4 --> returns the integer part of a number (removes decimal part)
+
+console.log(Math.pow(2, 3)); // 8 --> calculates 2 raised to the power of 3
+
+console.log(Math.sqrt(16)); // 4 --> calculates the square root of 16
+
+console.log(Math.max(1, 5, 3)); // 5 --> returns the largest of the given numbers
+
+console.log(Math.min(1, 5, 3)); // 1 --> returns the smallest of the given numbers
