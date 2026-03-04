d60ecad96308e80f6756f7d7b63a13d2cf4d0135
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-05 00:26:06 +0530

    triangle1

---
+++ Pattern/triangle1.js
@@ -0,0 +1,18 @@
+let prompt = require("prompt-sync")();
+
+let row = prompt("Enter number of row ");
+
+
+for (let i = 1; i <= row; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
