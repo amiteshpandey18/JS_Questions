b69d6d5a087e40f8f9214ca8e66de714c7ff51be
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-05 23:11:59 +0530

    -

--- Arrays/reverseArray.js
+++ Arrays/reverseArray.js
@@ -1,10 +1,26 @@
 let arr = [1, 2, 3, 4, 5, 6];
 
-let rarr = [];
-let j = 0;
+// let rarr = [];
+// let j = 0;
 
-for (let i = arr.length - 1; i >= 0; i--) {
-    rarr[j++] = arr[i];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     rarr[j++] = arr[i];
+// }
+
+// console.log("Reverse Array", rarr);
+
+// Using While Loop ------- (Best Approch) --------------------
+
+let i = 0;
+let j = arr.length - 1;
+
+
+while (i <= j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
 }
 
-console.log("Reverse Array", rarr);
+console.log("Reverse Array", arr);
