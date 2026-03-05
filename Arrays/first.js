bce3fa6b19c7174545aeea1848c387cd25c1c92d
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-05 15:31:42 +0530

    sumOfArray

---
+++ Arrays/first.js
@@ -0,0 +1,12 @@
+// let arr = new Array(3);
+
+// arr.push(10);
+
+// console.log(arr);  // [3 * empty, 10] // three empty index and on 4th index 10 is added
+
+
+let arr = new Array(5);
+
+for(let i =0;i<5;i++) {
+    arr[i] = Number(prompt("Enter value"));  // add element from the console
+}
