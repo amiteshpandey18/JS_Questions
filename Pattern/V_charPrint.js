25bb560c6dd5e29ca397d98d2568476c8c2e690b
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-05 13:04:08 +0530

    char V

---
+++ Pattern/V_charPrint.js
@@ -0,0 +1,21 @@
+let prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter n "));
+
+for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= n*2; j++) {
+        if(i==j || i + j == (n*2)) {
+            process.stdout.write("*");
+        } else {
+            process.stdout.write(" ");
+        }
+    }
+    console.log();
+}
+
+
+// *       * 
+//  *     *
+//   *   *
+//    * *
+//     *
