39acdfa46d7f422a6717375776b38d4fbe63011d
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-06 17:51:08 +0530

    Right roatation kth time

---
+++ Arrays/rightRotation.js
@@ -0,0 +1,25 @@
+let arr = [1, 2, 3, 4, 5];
+
+// let temp = arr[arr.length - 1];
+
+// for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+// }
+
+// arr[0] = temp;
+
+// console.log(arr);
+
+// rotation using user input  k times --------------
+
+let k = 2;
+
+for(let i = 0; i < k; i++) {
+     let temp = arr[arr.length-1];
+     for(let j = arr.length-1; j > 0; j--) {
+        arr[j] = arr[j-1]
+     }
+     arr[0] = temp;
+}
+
+console.log(arr);
