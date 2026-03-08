10e02695a8c87f55d179594befc832132eb5fd46
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-08 14:42:03 +0530

    third approch

--- Arrays/leftRotation.js
+++ Arrays/leftRotation.js
@@ -1,7 +1,7 @@
 let arr = [1, 2, 3, 4, 5];
 // let temp = arr[0];
 
-// for (let i = 1; i < arr.length; i++) {
+// for (let i = 1; i < arr.length; i++) {   // First approch
 //     arr[i - 1] = arr[i];
 // }
 // arr[arr.length - 1] = temp;
@@ -11,16 +11,27 @@ let arr = [1, 2, 3, 4, 5];
 
 // rotation using user input  k times -----------------
 
+// let k = 2;
+
+// k = k % arr.length; // Handle unneccesory rotation
+
+// for (let i = 0; i < k; i++) {            // Second Approch
+//    let temp = arr[0];                    
+//    for (let j = 1; j < arr.length; j++) {
+//       arr[j - 1] = arr[j]
+//    }
+//    arr[arr.length - 1] = temp;
+// }
+
+// rotation without double loop---------------------------------------
+
 let k = 2;
+k = k % arr.length;
 
-k = k % arr.length; // Handle unneccesory rotation
-
-for (let i = 0; i < k; i++) {
-   let temp = arr[0];
-   for (let j = 1; j < arr.length; j++) {
-      arr[j - 1] = arr[j]
-   }
-   arr[arr.length - 1] = temp;
+let temp = new Array(arr.length);
+for (let i = 0 ; i < arr.length; i++) {        // third Approch
+   temp[i] = arr[(i + k) % arr.length];
 }
 
-console.log(arr);
+
+console.log(temp);
