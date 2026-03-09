d7022e58cf93700750bc043291b53f579bcd83b7
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-09 21:42:20 +0530

    remove duplicate

---
+++ Arrays/removeDuplicate.js
@@ -0,0 +1,14 @@
+let arr = [1, 1, 2, 2, 3, 4, 4, 5];
+
+let j = 0;
+
+for (let i = 1; i < arr.length; i++) {
+
+    if (arr[i] != arr[j]) {
+        j++;
+        arr[j] = arr[i];
+    }
+}
+
+console.log(j + 1);   // number of unique elements
+console.log(arr.slice(0, j + 1));
