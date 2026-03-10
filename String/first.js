b8f534a9932a840489c523c7c859656b72cad722
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-10 20:03:03 +0530

    String start

---
+++ String/first.js
@@ -0,0 +1,17 @@
+let str = "Hello";
+
+// Strings behave like Array because it have indexing but they are not an actual array
+
+str2 = " world";
+
+console.log(str + str2); // concat string
+
+// String is immutable
+
+str + " world";
+console.log(str); // Hello
+
+let str3 = str + " world";
+console.log(str3); // Hello world
+
+console.log(str[2]); // l
