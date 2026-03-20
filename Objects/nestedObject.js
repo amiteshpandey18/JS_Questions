4335576b0c819dba4d95fc220530dcd76ac7ed93
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-20 20:47:37 +0530

    nested objects

---
+++ Objects/nestedObject.js
@@ -0,0 +1,13 @@
+let obj = {
+    name : "Amitesh",
+    details: {
+        roll : 23,
+        grade : 'A',
+        marks : 87
+    }
+}
+
+let key = "details";
+
+console.log(obj.details.grade);
+console.log(obj[key].roll);
---
+++ Objects/objects.js
@@ -0,0 +1,11 @@
+let obj = {
+    name : "Ajith",
+    age : 24
+}
+
+ console.log(obj.name);
+// console.log(obj["name"]); // access object key using brackets[]
+
+
+
+
