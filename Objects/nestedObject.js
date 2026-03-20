9e48c69e0c1633f2349e3e31059f40102c8ad7cf
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-21 01:19:53 +0530

    Nested and deep access

--- Objects/nestedObject.js
+++ Objects/nestedObject.js
@@ -11,3 +11,42 @@ let key = "details";
 
 console.log(obj.details.grade);
 console.log(obj[key].roll);
+console.log("---->",obj["name"]);
+
+// -----------------------------------------------------------------------------
+
+let student = {
+  name: "Amitesh",
+  details: {
+    roll: 23,
+    marks: {
+      math: 90,
+      science: 85
+    }
+  }
+};
+
+// 1. DOT Notation ------------------------------------------------------------
+
+console.log(student.details.roll); // 23
+console.log(student.details.marks.math);  // 90
+
+// 2. Bracket Notation --------------------------------------------------------
+
+console.log(student["details"]["marks"]["science"]); // 85
+
+
+// 3. Mixed Notation -----------------------------------------------------------
+
+let key1 = "details";
+console.log(student[key1].marks.math); // 90
+
+// 4. Optional Chaining (Safe Access) * -------------------------------------------
+
+console.log(student.details.marks.english); // 
+
+console.log(student.details?.english);  // Undefine
+
+     // Right way to right -->
+
+console.log(student.details?.english) // Undefine
