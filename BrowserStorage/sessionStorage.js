ac3669f898857e609923129a4cd232137c988c77
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-30 19:12:41 +0530

    Session Storage

---
+++ BrowserStorage/sessionStorage.js
@@ -0,0 +1,8 @@
+// store
+sessionStorage.setItem("token", "12345");  // store data only one browser tab session
+
+// get
+let token = sessionStorage.getItem("token");
+
+// remove
+sessionStorage.removeItem("token");
